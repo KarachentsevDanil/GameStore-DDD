9cd428554ec598189f6442f7a05fab0f1234b84e
Danylo Karachentsev <dkara@softserveinc.com>
2020-06-02 10:05:12 +0300

    Add logging to game

--- Shared/GSP.Shared.Utils/WebApi/Extensions/DbContextExtensions.cs
+++ Shared/GSP.Shared.Utils/WebApi/Extensions/DbContextExtensions.cs
@@ -3,6 +3,7 @@ using GSP.Shared.Utils.WebApi.Configurations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace GSP.Shared.Utils.WebApi.Extensions
 {
@@ -17,7 +18,11 @@ namespace GSP.Shared.Utils.WebApi.Extensions
             configuration.Bind(nameof(EntityFrameworkConfiguration), dbConfig);
             services.AddSingleton(dbConfig);
 
-            services.AddDbContext<TContext>(options => options.UseSqlServer(dbConfig.ConnectionString));
+            services.AddDbContext<TContext>(options =>
+                options
+                    .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+                    .UseSqlServer(dbConfig.ConnectionString));
+
             return services;
         }
     }
