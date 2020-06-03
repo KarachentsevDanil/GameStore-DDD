57b26ad4a6342ea079634da4cd22b9be70656c01
Danylo Karachentsev <dkara@softserveinc.com>
2020-06-03 10:05:16 +0300

    Remove useless method

--- Shared/GSP.Shared.Grid/Helpers/PredicateHelper.cs
+++ Shared/GSP.Shared.Grid/Helpers/PredicateHelper.cs
@@ -10,16 +10,12 @@ namespace GSP.Shared.Grid.Helpers
             return f => true;
         }
 
-        public static Expression<Func<TType, bool>> False<TType>()
-        {
-            return f => false;
-        }
-
         public static Expression<Func<TType, bool>> Or<TType>(
             this Expression<Func<TType, bool>> firstExpression,
             Expression<Func<TType, bool>> secondExpression)
         {
             InvocationExpression invokedExpr = Expression.Invoke(secondExpression, firstExpression.Parameters);
+
             return Expression.Lambda<Func<TType, bool>>(
                 Expression.OrElse(firstExpression.Body, invokedExpr), firstExpression.Parameters);
         }
